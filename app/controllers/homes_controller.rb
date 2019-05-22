2480167413e97a7c669576b723562f54c7077b30
Josh Young <j22cal@gmail.com>
2019-05-22 06:41:10 -0500

    Move methods to model

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,20 +1,6 @@
 class HomesController < ApplicationController
   def index
-    @net_http = JSON.parse(net_http)["slip"]["advice"]
-    @http_party = JSON.parse(http_party)["slip"]["advice"]
-  end
-
-  def net_http
-    uri = URI.parse("https://api.adviceslip.com/advice")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
-    response.body
-  end
-
-  def http_party
-    HTTParty.get("https://api.adviceslip.com/advice").response.body
+    @net_http = JSON.parse(Home.net_http)["slip"]["advice"]
+    @http_party = JSON.parse(Home.http_party)["slip"]["advice"]
   end
 end
--- app/models/home.rb
+++ app/models/home.rb
@@ -1,2 +1,17 @@
 class Home < ApplicationRecord
+  class << self
+    def net_http
+        uri = URI.parse("https://api.adviceslip.com/advice")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(uri.request_uri)
+        response = http.request(request)
+        response.body
+    end
+
+      def http_party
+          HTTParty.get("https://api.adviceslip.com/advice").response.body
+      end
+    end
 end
