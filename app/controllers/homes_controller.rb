1a8d4049fe16d0039a2dce3d805595708bb416ef
Josh Young <j22cal@gmail.com>
2019-05-21 19:49:55 -0500

    Basic api call with Net:HTTP

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,11 @@
 class HomesController < ApplicationController
   def index
+    uri = URI.parse("https://api.adviceslip.com/advice")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @advice = JSON.parse(response.body)["slip"]["advice"]
   end
 end
