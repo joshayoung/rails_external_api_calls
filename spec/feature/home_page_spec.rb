8019d6723b57b694b93e21af501b32b1df054143
Josh Young <j22cal@gmail.com>
2019-05-22 20:26:52 -0500

    Move tests to webmock

--- spec/feature/home_page_spec.rb
+++ spec/feature/home_page_spec.rb
@@ -2,6 +2,14 @@ require "rails_helper"
 
 RSpec.feature "Home Page", :type => :feature do
   scenario "Visit index page" do
+    stub_request(:get, "https://api.adviceslip.com/advice").
+    with(
+      headers: {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Ruby'
+      }).
+    to_return(status: 200, body: "{\"slip\": {\"advice\":\"Work is never as important as you think it is.\",\"slip_id\":\"74\"}}", headers: {})
     visit "/"
 
     expect(page).to have_text("Basic Rails API Usage")
--- spec/models/home_spec.rb
+++ spec/models/home_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe Home, type: :model do
   describe '.net_http' do
     context 'when calling .net_http' do
       it 'returns the correct result' do
-        advice = JSON.parse(Home.net_http("https://api.adviceslip.com/advice/1"))["slip"]["advice"]
+        stub_request(:get, "https://api.adviceslip.com/advice/3").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Ruby'
+          }).
+        to_return(status: 200, body: "{\"slip\": {\"advice\":\"Don't eat non-snow-coloured snow.\",\"slip_id\":\"74\"}}", headers: {})
 
-        expect(advice).to eq "Remember that spiders are more afraid of you, than you are of them."
+        advice = JSON.parse(Home.net_http("https://api.adviceslip.com/advice/3"))["slip"]["advice"]
+
+        expect(advice).to eq "Don't eat non-snow-coloured snow."
       end
     end
   end
@@ -14,6 +23,14 @@ RSpec.describe Home, type: :model do
   describe '.http_party' do
     context 'when calling .http_party' do
       it 'returns the correct result' do
+        stub_request(:get, "https://api.adviceslip.com/advice/2").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Ruby'
+          }).
+        to_return(status: 200, body: "{\"slip\": {\"advice\":\"Smile and the world smiles with you. Frown and you're on your own.\",\"slip_id\":\"74\"}}", headers: {})
         advice = JSON.parse(Home.http_party("https://api.adviceslip.com/advice/2"))["slip"]["advice"]
 
         expect(advice).to eq "Smile and the world smiles with you. Frown and you're on your own."
